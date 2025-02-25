6b9a41578b26e67ef27960d28d629c14760299da
Ivan <gomesp.ivan@gmail.com>
2025-02-24 23:54:01 -0300

    Exercicio

---
+++ Aula05/Eleicao.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Eleicao {
+    /*
+    Em uma cidade haverá uma eleição. Existem 3 candidatos:
+    1 – Huguinho
+    2 – Zezinho
+    3 – Luizinho
+
+    Diversos eleitores votarão; por não sabermos a quantidade a eleição será encerrada
+com a digitação de 0(zero) no voto. Implemente as seguintes rotinas neste
+programa:
+     a)ler os votos, acumular os votos de cada candidato e exibir a apuração.
+     b) Caso seja digitado um voto inválido (diferente de 0, 1, 2 e 3) acumular o
+        voto como nulo, exibindo a apuração.
+     c) Sabendo-se o total de votos, calcular a porcentagem atingida por cada
+        candidato e exibir.
+     d) Ao finalizar o algoritmo, exibir os candidatos – e as informações
+respectivas da apuração
+
+    APURAÇÃO
+    1.o –> 2 – Zezinho - 25 votos – 50%
+    2.o –> 1 – Huguinho - 20 votos – 40%
+    3.o –> 3 – Luizinho - 5 votos – 10%  * */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int qtdvotos = 0, zezinho = 0, huguinho = 0, luizinho = 0, nulos = 0, voto;
+
+        do {
+            System.out.println(" --ELEIÇÃO--" + "\n URNA ELETRÔNICA" + "\n Escolha o candidato que deseja votar de acordo com os números abaixo: " +
+                    "\n 1 – Huguinho" + "\n 2 – Zezinho" + "\n 3 – Luizinho" + "\n 0 - Encerrar eleição");
+            voto = input.nextInt();
+            qtdvotos++;
+            switch (voto){
+                case 1: huguinho++; break;
+                case 2: zezinho++; break;
+                case 3: luizinho++; break;
+                case 0: System.out.println("\n Eleiçao encerrada!"); break;
+                default: nulos++;
+            }
+        }while(voto!= 0);
+        int percentHuguinho = (huguinho / qtdvotos) * 100, percentZezinho = zezinho/qtdvotos*100;
+        int percentLuizinho = luizinho/qtdvotos*100, percentNulos = nulos/qtdvotos*100;
+
+        System.out.println("\n    APURAÇÃO"+
+                            "\n 1 - Huguinho - "+ huguinho +" votos - "+ percentHuguinho +"%"+
+                            "\n 2 - Zezinho  - "+ zezinho + " votos - "+ percentZezinho +"%"+
+                            "\n 3 - Luizinho - "+ luizinho +" votos - "+ percentLuizinho +"%"+
+                            "\n # - Nulos    - "+ nulos +   " votos - "+ percentNulos+"%");
+    }
+}

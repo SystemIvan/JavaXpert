1cfd186b16ee819b5094862ad8fd2e13a5251ad3
Ivan <gomesp.ivan@gmail.com>
2025-02-26 00:17:39 -0300

    For-each/Constante/vetorLength

--- Aula06/Exercicio.java
+++ Aula06/Exercicio.java
@@ -2,28 +2,33 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exercicio {
-    //1. Criar um vetor com 6 posições;
-    //1. Preencher o vetor;
-    //1. Exibam o maior valor
-    //1. Média dos valores
+    //1. Criar um vetor com qtdNumeros posições;
+    //2. Preencher o vetor;
+    //3. Exibam o maior valor;
+    //4. Média dos valores.
+
+    //CONSTANTE
+    public static final int qtdNumeros = 4;
+
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0.00");
         Scanner input = new Scanner(System.in);
 
-        double[] vetor = new double[6];
-        double media = 0, soma = 0, maior = -1;
+        double[] vetor = new double[qtdNumeros]; //1. Criar um vetor com qtdNumeros posições;
+        double media = 0, soma = 0;
 
-        for (int i = 0; i < 6; i++) {
-            System.out.println("Digite o " + (i + 1)+ " valor:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Digite o " + (i + 1) + " valor:"); //2. Preencher o vetor;
             vetor[i] = input.nextDouble();
             soma += vetor[i];
         }
-        for (int i = 0; i < 6; i++) {
-            if (vetor[i] > maior) {
-                maior = vetor[i];
+        double maior = vetor[0];
+        for (double v : vetor) {
+            if (v > maior) {  //3. Exibam o maior valor;
+                maior = v;
             }
         }
         System.out.println("\nO maior valor é " + maior +
-                "\nA média dos valores é: " + df.format(media = soma / 6));
+                "\nA média dos valores é: " + df.format(media = soma / qtdNumeros)); //4. Média dos valores.
     }
 }

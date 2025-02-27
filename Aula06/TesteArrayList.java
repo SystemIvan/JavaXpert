1b7f395caad1de7fabe4eb9d387146d0785b717f
Ivan <gomesp.ivan@gmail.com>
2025-02-26 23:50:38 -0300

    Listas

--- Aula06/TesteArrayList.java
+++ Aula06/TesteArrayList.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TesteArrayList {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int[] vetor = new int[10];
+        List<String> listaNomes = new ArrayList<>();
+        List<Integer> listaNumeros = new ArrayList<>();
+
 
     }
 }

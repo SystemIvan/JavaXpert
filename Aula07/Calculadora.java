ba960f616a1f1ee6a5e08a02642c705eeeb796aa
Ivan <gomesp.ivan@gmail.com>
2025-02-28 00:18:21 -0300

    Programar é muito divertido! Funcao part 1

--- Aula07/Calculadora.java
+++ Aula07/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
     //PROCEDIMENTO     Procedimento => void
     //Sintaxe Procedimento sem argumento => public static void nomeProcedimento(){}
     //Sintaxe Procedimento com argumento => public static void nomeProcedimento(tipo nomeVariavel){}
-    public static void exibirMenu(){
+    public static void exibirMenu() {
         System.out.println("\nPROGRAMA CALCULADORA");
         System.out.println("---------------------------------");
         System.out.println("Escolha sua opção: ");
@@ -20,45 +20,52 @@ public class Calculadora {
         System.out.println("6 - Sair");
     }
 
+    public static void exibirResultado() {
+        System.out.println("O resultado é " + resultado);
+    }
+
+    public static void exibirResultado(double resultado) {
+        System.out.println("O resultado é " + resultado);
+    }
+
+    public static void exibirResultado(double resultado, String operacao) {
+        System.out.println(operacao + " é igual a " + resultado);
+    }
+
+    public static void somar(double primeiroValor, double segundoValor) {
+        double soma;
+        System.out.println("\n\nRealizando a soma entre " + primeiroValor + " e " + segundoValor);
+        soma = primeiroValor + segundoValor;
+        exibirResultado(soma, "Soma");
+    }
+
     //FUNÇÃO  =>  tem retorno
     //Sintaxe Procedimento sem argumento => public static tipo nomeFuncao(){}
     //Sintaxe Procedimento com argumento => public static tipo nomeFuncao(tipo nomeVariavel){}
     //TIPO : int, double, String, vetor, Lista
     //SEMPRE TEM RETORN => return x;
 
-    public static void exibirResultado(){
-        System.out.println("O resultado é " + resultado );
-    }
+    // FUNCAO
 
-    public static void exibirResultado(double resultado){
-        System.out.println("O resultado é " + resultado );
-    }
-
-    public static void exibirResultado(double resultado, String operacao){
-        System.out.println(operacao + " é igual a "+ resultado );
-    }
-
-    public static void somar(double primeiroValor, double segundoValor){
-        double soma;
-        System.out.println("\n\nRealizando a soma entre " + primeiroValor + " e " + segundoValor);
-        soma = primeiroValor +  segundoValor;
-        exibirResultado(soma, "Soma");
+    public static double subtrair(double v1, double v2) {
+        System.out.println("\n\nRealizando a subtração entre " + v1 + " e " + v2);
+        return v1 - v2;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Scanner leitor = new Scanner(System.in);
-        double valor1 = 0, valor2=0;
-       // double resultado; variavel
+        double valor1 = 0, valor2 = 0;
+        // double resultado; variavel
         double result = 0;
 
         int opcao = 0;
 
-        while (opcao!=6) {
+        while (opcao != 6) {
             exibirMenu();
             opcao = leitor.nextInt();
 
-            switch(opcao) {
+            switch (opcao) {
                 case 1:
                     System.out.println("\n\nDigite o 1. valor:");
                     valor1 = leitor.nextDouble();
@@ -67,22 +74,24 @@ public class Calculadora {
                     System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados\n\n");
                     break;
                 case 2:
-                    somar(valor1,valor2);
+                    somar(valor1, valor2);
                     break;
                 case 3:
-                    result = valor1 -  valor2;
-                    System.out.println("\n\nRealizando a subtração entre " + valor1 + " e " + valor2);
-                    exibirResultado(result);
+                    // double subtracao = subtrair(valor1,valor2);
+                    // System.out.println("\n\nRealizando a subtração entre " + valor1 + " e " + valor2);
+                    // exibirResultado(result);
+                    //  exibirResultado(subtracao);
+                    exibirResultado(subtrair(valor1, valor2), "Subtração");
 
                     break;
                 case 4:
-                    resultado = valor1 /  valor2;
+                    resultado = valor1 / valor2;
                     System.out.println("\n\nRealizando a divisão entre " + valor1 + " e " + valor2);
                     exibirResultado();
 
                     break;
                 case 5:
-                    resultado = valor1 *  valor2;
+                    resultado = valor1 * valor2;
                     System.out.println("\n\nRealizando a multiplicação entre " + valor1 + " e " + valor2);
                     exibirResultado();
 
@@ -92,10 +101,6 @@ public class Calculadora {
                     break;
             }
         }
-
-
         leitor.close();
-
-
     }
 }

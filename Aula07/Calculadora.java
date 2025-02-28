fe6adf180ab1605d30e567e64e9300db618cc763
Ivan <gomesp.ivan@gmail.com>
2025-02-27 23:48:02 -0300

    Métodos e funções com e sem argumentos

---
+++ Aula07/Calculadora.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    //Variavel Global
+    public static double resultado;
+
+    //PROCEDIMENTO     Procedimento => void
+    //Sintaxe Procedimento sem argumento => public static void nomeProcedimento(){}
+    //Sintaxe Procedimento com argumento => public static void nomeProcedimento(tipo nomeVariavel){}
+    public static void exibirMenu(){
+        System.out.println("\nPROGRAMA CALCULADORA");
+        System.out.println("---------------------------------");
+        System.out.println("Escolha sua opção: ");
+        System.out.println("1 - Digitar valores");
+        System.out.println("2 - Realizar soma");
+        System.out.println("3 - Realizar subtração");
+        System.out.println("4 - Realizar divisão");
+        System.out.println("5 - Realizar multiplicação");
+        System.out.println("6 - Sair");
+    }
+
+    //FUNÇÃO     Procedimento => void
+    //Sintaxe Procedimento sem argumento => public static void nomeProcedimento(){}
+    //Sintaxe Procedimento com argumento => public static void nomeProcedimento(tipo nomeVariavel){}
+
+    public static void exibirResultado(){
+        System.out.println("O resultado é " + resultado );
+    }
+
+    public static void exibirResultado(double resultado){
+        System.out.println("O resultado é " + resultado );
+    }
+
+    public static void exibirResultado(double resultado, String operacao){
+        System.out.println(operacao + " é igual a "+ resultado );
+    }
+
+    public static void somar(double primeiroValor, double segundoValor){
+        double soma;
+        System.out.println("\n\nRealizando a soma entre " + primeiroValor + " e " + segundoValor);
+        soma = primeiroValor +  segundoValor;
+        exibirResultado(soma, "Soma");
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner leitor = new Scanner(System.in);
+        double valor1 = 0, valor2=0;
+       // double resultado; variavel
+        double result = 0;
+
+        int opcao = 0;
+
+        while (opcao!=6) {
+            exibirMenu();
+            opcao = leitor.nextInt();
+
+            switch(opcao) {
+                case 1:
+                    System.out.println("\n\nDigite o 1. valor:");
+                    valor1 = leitor.nextDouble();
+                    System.out.println("\n\nDigite o 2. valor:");
+                    valor2 = leitor.nextDouble();
+                    System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados\n\n");
+                    break;
+                case 2:
+                    somar(valor1,valor2);
+                    break;
+                case 3:
+                    result = valor1 -  valor2;
+                    System.out.println("\n\nRealizando a subtração entre " + valor1 + " e " + valor2);
+                    exibirResultado(result);
+
+                    break;
+                case 4:
+                    resultado = valor1 /  valor2;
+                    System.out.println("\n\nRealizando a divisão entre " + valor1 + " e " + valor2);
+                    exibirResultado();
+
+                    break;
+                case 5:
+                    resultado = valor1 *  valor2;
+                    System.out.println("\n\nRealizando a multiplicação entre " + valor1 + " e " + valor2);
+                    exibirResultado();
+
+                    break;
+                case 6:
+                    System.out.println("Saindo do sistema");
+                    break;
+            }
+        }
+
+
+        leitor.close();
+
+
+    }
+}

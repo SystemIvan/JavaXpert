94849c09c04059aa0f8a5f309952114a0975ac3d
Ivan <gomesp.ivan@gmail.com>
2025-02-28 23:49:25 -0300

    Procedimentos- vetor para adicionar itens no carrinho

--- Aula07/CarrinhoCompras.java
+++ Aula07/CarrinhoCompras.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class CarrinhoCompras {
 
+    public static void somarCarrinho(double[] produtos){
+
+    }
+    public static void adicionarItens(){
+
+    }
+
     public static void exibirMenu(){
+        Scanner input = new Scanner(System.in);
+        double[] valoresItens = new double[10];
         System.out.println("LOJINHA");
         System.out.println("1 - Adicionar valor do produto no carrinho ");
         System.out.println("2 - Exibir o total do carrinho");
         System.out.println("3 - Sair");
+
+        //adicionar itens no vetor
+        for (int i = 0; i<valoresItens.length; i++){
+            System.out.println("Digite o valor do item: ");
+
+        }
     }
     public static void main(String[] args) {
 

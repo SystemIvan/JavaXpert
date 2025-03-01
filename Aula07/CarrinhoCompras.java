bc9e3f1247acc8a7fbd4cfa62ebda9c56ea1aa10
Ivan <gomesp.ivan@gmail.com>
2025-02-28 23:42:23 -0300

    Procedimento - ExibirMenu

--- Aula07/CarrinhoCompras.java
+++ Aula07/CarrinhoCompras.java
@@ -1,4 +1,11 @@
 public class CarrinhoCompras {
+
+    public static void exibirMenu(){
+        System.out.println("LOJINHA");
+        System.out.println("1 - Adicionar valor do produto no carrinho ");
+        System.out.println("2 - Exibir o total do carrinho");
+        System.out.println("3 - Sair");
+    }
     public static void main(String[] args) {
 
     }

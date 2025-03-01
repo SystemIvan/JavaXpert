532abd56b55315edf5261ab7f54efc999ad384e2
Ivan <gomesp.ivan@gmail.com>
2025-03-01 00:19:18 -0300

    Carrinho compras - Funcionamento completo parte 1

--- Aula07/CarrinhoCompras.java
+++ Aula07/CarrinhoCompras.java
@@ -2,28 +2,47 @@ import java.util.Scanner;
 
 public class CarrinhoCompras {
 
-    public static void somarCarrinho(double[] produtos){
-
+    public static double somarCarrinho(double[] produtos){
+        double soma = 0;
+        for(double i : produtos){
+            soma += i;
+        }
+        return soma;
     }
-    public static void adicionarItens(){
-
-    }
+    // public static void adicionarItens(){}
 
     public static void exibirMenu(){
-        Scanner input = new Scanner(System.in);
-        double[] valoresItens = new double[10];
         System.out.println("LOJINHA");
         System.out.println("1 - Adicionar valor do produto no carrinho ");
         System.out.println("2 - Exibir o total do carrinho");
         System.out.println("3 - Sair");
-
-        //adicionar itens no vetor
-        for (int i = 0; i<valoresItens.length; i++){
-            System.out.println("Digite o valor do item: ");
-
-        }
+        System.out.println("Digite a opção desejada: ");
     }
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        double[] valoresItens = new double[5];
+        int op;
+
+        do {
+            exibirMenu();
+            op = input.nextInt();
+            switch (op) {
+                //adicionar itens no vetor
+                case 1:
+                    for (int i = 0; i < valoresItens.length; i++) {
+                        System.out.println("Digite o valor do item: ");
+                        valoresItens[i] = input.nextDouble();
+                    }
+                    break;
+                case 2:
+                    System.out.println("O total do carrinho é R$ " + somarCarrinho(valoresItens));
+                    break;
+                case 3:
+                    System.out.println("Sistema finalizado!");
+                    break;
+
+            }
+        }while( op != 3);
     }
 }

ec1335a15d04d96c8eef167758993db2f60dfff7
Ivan <gomesp.ivan@gmail.com>
2025-03-03 18:19:21 -0300

    Resolvendo alguns Warnings

--- Aula08/Loja.java
+++ Aula08/Loja.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,13 +10,21 @@ public class Loja {
 
     public static List<String> listaProdutos = new ArrayList<>();
 
-    public static void registrarProduto(){
+    public static void registrarProduto() {
         Scanner inputText = new Scanner(System.in);
         System.out.println("Digite o nome do produto que deseja registrar: ");
         String nomeProduto = inputText.nextLine();
         listaProdutos.add(nomeProduto);
         System.out.println("O produto " + nomeProduto + " foi registrado com sucesso! ");
 
+    }
+
+    public static String mostrarProdutos() {
+        String mensagem = "Produtos: \n";
+        for (String produto : listaProdutos){
+            mensagem += produto + "\n";
+        }
+        return mensagem;
     }
 
     public static void registrarUsuario() {
@@ -35,12 +42,18 @@ public class Loja {
         apelido = input.nextLine();
     }
 
-    public static boolean validarLogin(String emailLogin, String senhaLogin) {
-        if (emailLogin.equalsIgnoreCase(email) && senhaLogin.equals(senha)) {
-            return true;
-        }
-        return false;
+    private static void subMenu() {
+        System.out.println("\nUsuário logado com Sucesso!!");
+        System.out.println("Digite 1 para registrar um produto");
+        System.out.println("Digite 2 para mostrar produtos");
+        System.out.println("Digite 3 para avaliar um produto");
+        System.out.println("Digite 0 para retornar ao menu principal");
+        System.out.println("Digite a opção desejada: ");
+    }
 
+
+    public static boolean validarLogin(String emailLogin, String senhaLogin) {
+        return emailLogin.equalsIgnoreCase(email) && senhaLogin.equals(senha);
     }
 
     public static void main(String[] args) {
@@ -48,13 +61,13 @@ public class Loja {
         Scanner input = new Scanner(System.in);
 
         int opcao;
-        do{
+        do {
             System.out.println("\n---SYSTEM IVAN---");
             System.out.println("1 - Cadastrar Usuário");
             System.out.println("2 - Efetuar Login ");
             System.out.println("3 - Sair");
             opcao = input.nextInt();
-            if (opcao == 1){
+            if (opcao == 1) {
                 registrarUsuario();
             } else if (opcao == 2) {
                 System.out.println("\n---LOGIN---");
@@ -62,9 +75,9 @@ public class Loja {
                 String emailDigitado = inputText.nextLine();
                 System.out.println("Digite sua senha: ");
                 String senhaDigitada = inputText.nextLine();
-                if (validarLogin(emailDigitado,senhaDigitada)){
+                if (validarLogin(emailDigitado, senhaDigitada)) {
                     System.out.println("\n---Bem vindo ao Sistema do Ivan " + apelido);
-                    int opcaoSubMenu = 0;
+                    int opcaoSubMenu;
                     do {
                         subMenu();
                         opcaoSubMenu = input.nextInt();
@@ -73,7 +86,7 @@ public class Loja {
                                 registrarProduto();
                                 break;
                             case 2:
-
+                                System.out.println(mostrarProdutos());
                                 break;
                             case 3:
                                 System.out.println("Avaliar Produto");
@@ -82,24 +95,15 @@ public class Loja {
                                 System.out.println("Voltando ao Menu Principal...");
                                 break;
                         }
-                    }while(opcaoSubMenu != 0);
-                }else {
+                    } while (opcaoSubMenu != 0);
+                } else {
                     System.out.println("Usuário e/ou senha inválidos");
                 }
             }// opcao 2
             else {
                 System.out.println("Obrigado por utilizar nosso sistema!");
             }
-        }while(opcao != 3);
+        } while (opcao != 3);
 
-    }
-
-    private static void subMenu() {
-        System.out.println("\nUsuário logado com Sucesso!!");
-        System.out.println("Digite 1 para registrar um produto");
-        System.out.println("Digite 2 para mostrar um produto");
-        System.out.println("Digite 3 para avaliar um produto");
-        System.out.println("Digite 0 para retornar ao menu principal");
-        System.out.println("Digite a opção desejada: ");
     }
 }

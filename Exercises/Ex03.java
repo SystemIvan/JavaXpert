49e2fee001f72a9e5fd25f298e5ab61b2e324971
Ivan <gomesp.ivan@gmail.com>
2025-03-05 22:45:30 -0300

    Verificar se o número é positivo ou não

--- Exercises/Ex03.java
+++ Exercises/Ex03.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Ex03 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n = input.nextInt();
+
+
 
     }
 }

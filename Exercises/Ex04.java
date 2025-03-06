aa33846ce34fee3c8c2e590997c16cb3dff2784e
Ivan <gomesp.ivan@gmail.com>
2025-03-05 22:52:36 -0300

    Ex04

---
+++ Exercises/Ex04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n = input.nextInt();
+
+        if (n < 0) {
+            System.out.println("O número é negativo!");
+        }else if {
+            (n > 0)
+        }System.out.println("O número é positivo! ");
+    }else{
+        System.out.println("O número ");
+    }
+}

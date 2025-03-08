422233535c1016ce2fce6b7e9253312fcbaa8616
Ivan <gomesp.ivan@gmail.com>
2025-03-07 22:58:29 -0300

    Criando um objeto

--- DomainDrivenDesign/fiap-ddd-livraria/LojaVirtualTeste.java
+++ DomainDrivenDesign/fiap-ddd-livraria/LojaVirtualTeste.java
@@ -1,3 +1,12 @@
 public class LojaVirtualTeste {
 
+    public static void main(String[] args) {
+        // Criando Objetos
+        // 1- declare uma variavel para a referencia -> Livro primeiroLivro
+        // 2-Crie um Objeto -> new Livro()
+        // 3-Associa o objeto e a referência -> =
+        Livro primeiroLivro = new Livro();
+
+        primeiroLivro.titulo = "O Hobbit";
+    }
 }

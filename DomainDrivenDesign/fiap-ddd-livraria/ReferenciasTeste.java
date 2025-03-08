7fc71ab1ecc99c86fb5696b073d2bed2ca341f1c
Ivan <gomesp.ivan@gmail.com>
2025-03-07 23:29:11 -0300

    Referencias ao objetos

--- DomainDrivenDesign/fiap-ddd-livraria/ReferenciasTeste.java
+++ DomainDrivenDesign/fiap-ddd-livraria/ReferenciasTeste.java
@@ -2,6 +2,19 @@ public class ReferenciasTeste {
     public static void main(String[] args) {
         Livro livroA = new Livro();
         livroA.titulo = "Teste";
-        System.out.println("Título: " + livroA.titulo);
+        System.out.println("Título A: " + livroA.titulo);
+
+        // Livro livroB = livroA;
+        Livro livroB = new Livro();
+        System.out.println("Título B: " + livroB.titulo);
+
+        livroB.titulo = "Teste alterado";
+        System.out.println("Título B: " + livroB.titulo);
+
+        System.out.println("Título A: " + livroA.titulo);
+
+        System.out.println(livroA);
+        System.out.println(livroB);
+
     }
 }

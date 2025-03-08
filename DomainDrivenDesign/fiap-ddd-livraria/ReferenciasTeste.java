913cc710422d7d072a59662319f31f86fdb3f375
Ivan <gomesp.ivan@gmail.com>
2025-03-07 23:17:35 -0300

    new class

---
+++ DomainDrivenDesign/fiap-ddd-livraria/ReferenciasTeste.java
@@ -0,0 +1,7 @@
+public class ReferenciasTeste {
+    public static void main(String[] args) {
+        Livro livroA = new Livro();
+        livroA.titulo = "Teste";
+        System.out.println("Título: " + livroA.titulo);
+    }
+}

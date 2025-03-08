47f0466bf5b0068b0dfb4f5f2a857611b1e26a60
Ivan <gomesp.ivan@gmail.com>
2025-03-08 17:32:02 -0300

    metodo com retorno dentro da classe Livro

--- DomainDrivenDesign/fiap-ddd-livraria/Livro.java
+++ DomainDrivenDesign/fiap-ddd-livraria/Livro.java
@@ -5,12 +5,18 @@ public class Livro {
     double preco;
     int paginas;
 
-    void exibir(){     //metódo
-        System.out.println("\nTítulo: " + titulo);
+    void exibir(){     //metódo sem retorno
+        System.out.println("\nDetalhes do livro: " + titulo);
+        System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
-        System.out.println("Preço: " + preco);
+        System.out.println("Preço: " + formatarPreco());
         System.out.println("Capa: " + tipoCapa);
         System.out.println("Páginas: " + paginas);
+        System.out.println("---------------------------------");
+    }
 
+    String formatarPreco(){ // metódo com retorno
+        String resultado = String.format("R$ %.2f", preco);
+        return resultado;
     }
 }
